c96eb50192909b5bfc581a8fe214622dae478f3e
phidangthanh <dtphi.khtn@gmail.com>
2020-09-23 15:28:35 +0700

    init config and change home page

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
 import { createStore } from 'vuex'
 import home from './home'
+import rs from './resources'
 
 export default createStore({
   state: {
+    cfApp: rs
+  },
+  getters: {
+    cfApp(state) {
+      return state.cfApp
+    }
   },
   mutations: {
   },
--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,12 @@ module.exports = {
   productionSourceMap: false,
   outputDir: path.resolve(__dirname, "../../public/lcg-front"),
   indexPath: path.resolve(__dirname, "../views/lcg_front/index.blade.php"),
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+      args[0].title = 'Lịch Công Giáo';
+      return args;
+    });
+  }
 };
